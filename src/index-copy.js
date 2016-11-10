76748aa6854fc4e98d0dbed368713af0ba36f3e7
Sam Pellegrene <pellegrenesam@gmail.com>
2016-11-10 14:48:41 -0600

    constructor assignment done

--- src/index-copy.js
+++ src/index-copy.js
@@ -31,11 +31,32 @@ Array.prototype.groupBy = function(callback){
       return newGroups;
 }
 
-
-
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var letterTypes = letters.groupBy(function(v, i, arr){
+  return (v === "A" || v === "E" || v === "I" || v=== "O" || v=== "U")? 'vowel' : 'consonant';
+})
+console.log (letterTypes);
 var groups = [1,2,3,4,5,6,7,8,9,10].groupBy(function(v, i, arr){
      return (v%2 === 0) ? 'even' : 'odd'
+  // if (v%2 === 0){
+  //   return 'even';
+  // } else{
+  //   return 'odd';
+  // }
 })
 
 console.log(groups);
 //--> { odd: [1,3,5,7,9], even: [2,4,6,8,10] }
+
+var peeps = [1,2,3,4,5,6,7,8,9,10].groupBy(function(v, i, arr){
+    //  return (v%2 === 0) ? 'even' : 'odd'
+  if ((v%3) === 0){
+    return 'divisible3';
+  } else if (v%2 === 0){
+    return 'divisible2';
+  } else{
+    return 'indivisble';
+  }
+})
+
+console.log(peeps);
